998ff98f57a2bcaac602c9070afc09e3a26f37aa
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-04-23 21:17:35 -0300

    feat: Add FeaturesComponent

---
+++ src/types.ts
@@ -0,0 +1,54 @@
+import { IConfigComponent, IFetchComponent, ILoggerComponent } from "@well-known-components/interfaces"
+
+export type FeatureFlagVariant = {
+  name: string
+  payload: {
+    type: string
+    value: string
+  }
+  enabled: boolean
+}
+
+export type FeaturesFlagsResponse = {
+  flags: Record<string, boolean>
+  variants: Record<string, FeatureFlagVariant>
+}
+
+export type FeaturesComponents = {
+  fetch: IFetchComponent
+  logs: ILoggerComponent
+  config: IConfigComponent
+}
+
+export type IFeaturesComponent = {
+  /**
+   * Helper to get whether a feature flag is enabled or disabled.
+   * It will first look into your env file for the feature flag, if it is not defined there,
+   * it will look it in the requested and stored features data.
+   * The env key will be determined from the application and the flag. For example, if the
+   * application is "explorer" and the flag is "some-crazy-feature", it will look
+   * for it as FF_EXPLORER_SOME_CRAZY_FEATURE.
+   * @param app Appplication name.
+   * @param feature Feature key without the application name prefix. For example for the "builder-feature".
+   * @returns Whether the feature is enabled or not and its variant.
+   */
+  getEnvFeature(app: string, feature: string): Promise<string | undefined>
+  getIsFeatureEnabled(app: string, feature: string): Promise<boolean>
+  getFeatureVariant(app: string, feature: string): Promise<FeatureFlagVariant | null>
+}
+
+export enum ApplicationName {
+  EXPLORER = "explorer",
+  BUILDER = "builder",
+  MARKETPLACE = "marketplace",
+  ACCOUNT = "account",
+  DAO = "dao",
+  DAPPS = "dapps",
+  EVENTS = "events",
+  LANDING = "landing",
+  TEST = "test",
+}
+
+export type TestComponents = FeaturesComponents & {
+  features: IFeaturesComponent
+}
